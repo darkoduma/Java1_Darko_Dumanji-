684d7a495bde428ea302a6c1b9d1d29eda648dc9
Darko <darko.dumanjic@gmail.com>
2018-06-08 11:23:54 +0200

    Java_Darko_Dumanji- nastavak

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 
 package main;
 
+import media.Radio;
 import media.Television;
 
 
@@ -9,13 +10,17 @@ public class Main {
     
     public static void main(String [] args) {
         
-        Television myTv = new Television();
-        System.out.println();
-        myTv.setVolume(3);
-        myTv.setTurnOn(true);
-        myTv.setCurrentProgram(10);
+//        Television myTv = new Television();
+//        System.out.println();
+//        myTv.setVolume(3);
+//        myTv.setTurnOn(true);
+//        myTv.setCurrentProgram(10);
+//        
+//        myTv.showData();
+        Radio myRadio = new Radio();
+        System.out.println("");
+        myRadio.showData();
         
-        myTv.showData();
         
     }
     
--- src/media/Radio.java
+++ src/media/Radio.java
@@ -1,14 +1,12 @@
-
 package media;
 
-
 public class Radio {
-    
+
     private double fmFrequency;
     private double amFrequency;
     private char band;
-    
-    public int getFmFrequency() {
+
+    public double getFmFrequency() {
         return this.fmFrequency;
     }
 
@@ -19,33 +17,42 @@ public class Radio {
     public double getAmFrequency() {
         return this.amFrequency;
     }
-    
+
     public void setAmFrequency(int amFrequency) {
         this.amFrequency = amFrequency;
-        
+
     }
-    
+
     public char getBand() {
         return this.band;
-        
+
     }
-    
+
     public void setBand(char band) {
         this.band = band;
-        
+
     }
-    
-    public Radio(){
+
+    public Radio() {
         this.fmFrequency = 106.8;
         this.amFrequency = 600;
         this.band = 'D';
-        
-        
-    }
-     
-    
-   
-   
-    
-}
 
+    }
+
+    public void printAtributes() {
+        System.out.println("FM Frekvencija je: " + this.fmFrequency);
+        System.out.println("AM Frekvencija je: " + this.amFrequency);
+        System.out.println("Band je: " + band);
+        System.out.println("");
+    }
+
+    public void showData() {
+        System.out.println("FM Frekvencija je: " + this.fmFrequency);
+        System.out.println("AM Frekvencija je: " + this.amFrequency);
+        System.out.println("Band je: " + band);
+        System.out.println("");
+
+    }
+
+}
